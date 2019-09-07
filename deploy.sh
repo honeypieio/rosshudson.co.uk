6dade74d841cc914541244b74cbaaed67ef2a5e4
Ross Hudson <hello@rosshudson.co.uk>
2019-09-07 22:10:33 +0100

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo git pull
+sudo git subtree pull --prefix=public https://github.com/honeypieio/rosshudson.co.uk.git gh-pages
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
