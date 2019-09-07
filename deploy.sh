1b370195607dd98d64eb355cb268962c482997e9
Ross Hudson <hello@rosshudson.co.uk>
2019-09-07 22:50:28 +0100

    Rebuilding site Sat  7 Sep 22:50:28 BST 2019!

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo git pull
-sudo git subtree pull --prefix=public https://github.com/honeypieio/rosshudson.co.uk.git gh-pages
+git pull
+git subtree pull --prefix=public https://github.com/honeypieio/rosshudson.co.uk.git gh-pages
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
@@ -9,7 +9,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo
 
 # Add changes to git.
-sudo git add -A
+git add -A
 
 # Commit changes.
 msg="Rebuilding site `date`!"
@@ -17,8 +17,8 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 
-sudo git commit -m "$msg"
+git commit -m "$msg"
 
 # Push source and build repos.
-sudo git push origin master
-sudo git push origin `sudo git subtree split --prefix=public master`:gh-pages --force
+git push origin master
+git push origin `git subtree split --prefix=public master`:gh-pages --force
